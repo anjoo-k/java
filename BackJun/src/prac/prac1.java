1d40ffffbbf444f76addf2af33234a13be7b5767
anjoo-k <knjou92@naver.com>
2024-03-13 20:44:07 +0900

    no message

---
+++ BackJun/src/prac/prac1.java
@@ -0,0 +1,17 @@
+package prac;
+
+import java.util.Scanner;
+
+public class prac1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        System.out.println(str.repeat(n));
+        
+       
+	    }
+	}
+
+
