78d37a7ab660503bc91c80351238961d5c1dbf74
anjoo-k <knjou92@naver.com>
2024-03-20 21:06:40 +0900

    no message

--- BackJun/src/prac/prac1.java
+++ BackJun/src/prac/prac1.java
@@ -10,8 +10,18 @@ public class prac1 {
 //        int n = sc.nextInt();
 //        System.out.println(str.repeat(n)); 여기엔 없는듯?
 		
-		
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for(int i = 0; i < a.length(); i++) {
+        	if(a.charAt(i) >= 97 && a.charAt(i) <= 122) {
+        		System.out.println(a.toUpperCase().charAt(i));
+        	} else {
+        		System.out.println(a.toLowerCase().charAt(i));
+        	}
+        }
         
+        System.out.println("!");
        
 	    }
 	}
